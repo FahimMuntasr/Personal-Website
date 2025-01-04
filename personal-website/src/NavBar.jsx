1bfac94ff8ef4e68551b96e72245f95737f47163
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-01-04 20:59:38 +0600

    Navbar hamburger menu

--- personal-website/src/NavBar.jsx
+++ personal-website/src/NavBar.jsx
@@ -1,10 +1,16 @@
 import './NavBar.css'
+import { useState } from 'react'
 import DarkModeSwitch from './DarkModeSwitch.jsx'
 export default function NavBar(){
+    const[menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
     return(
         <div id="NavBar-Container">
             <a href="#" id="NavBar-Name-button" className='navbar-buttons'>FAHIM MUNTASIR</a>
-            <div id="NavBar-Items-Container">
+            <div className='hamburger-menu' onClick={toggleMenu}>☰</div>
+            <div id="NavBar-Items-Container" className={menuOpen ? "open" : ""}>
                 <a href="#AboutMe-container"className='navbar-buttons'>ABOUT ME</a>
                 <a href="#Skill-container"className='navbar-buttons'>SKILLS</a>
                 <a href="#Education-Container"className='navbar-buttons'>EDUCATION</a>
