7a350b71cb1e4e7a7b7513fc396599af26ae0d63
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-01-05 23:17:12 +0600

    Final build V_1.0

--- personal-website/src/Footer.jsx
+++ personal-website/src/Footer.jsx
@@ -1,20 +1,19 @@
+import './Footer.css'
+import DarkModeSwitch from './DarkModeSwitch' 
+import ContactButtons from './ContactButtons'
 export default function Footer(){
     return(
-        <div>
-            <div>
-                <small>© 2025 Fahim Muntasir - made using ReactJS</small>
-            </div>
-            <div>
-                <div>
-                    <ul>
-                        <li>Linkedin</li>
-                        <li>Github</li>
-                        <li>Mail</li>
-                    </ul>
+        <div className='Footer-container'>
+            <small>© 2025 Fahim Muntasir - made using ReactJS</small>
+            <div id='buttons-container'>
+                <div id='contact'>
+                    <ContactButtons/>
                 </div>
-                <div>
-                    <button>Top of Page button</button>
-                    <button>Dark Mode</button>
+                <div id='ui-buttons'>
+                    <a href='#'>
+                        <img id='top-image' src='up.png'/>
+                    </a>
+                    <DarkModeSwitch/>
                 </div>
             </div>
         </div>
