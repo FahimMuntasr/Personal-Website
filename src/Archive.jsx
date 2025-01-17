df19e514d841de46ce65f3dd482bb98a444299bc
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-01-17 21:18:41 +0600

    Add cse225 and eee141 courses

--- src/Archive.jsx
+++ src/Archive.jsx
@@ -5,17 +5,24 @@ export default function Archive(){
             <h2>Archive</h2>
             <div id="Archive">
                 <div  className="course glass">
-                    <a href="https://drive.google.com/file/d/11iiwKEPxLeY-QmJ4_EnOrYBoCjaW6GWK/view?usp=drive_link" target="_blank" className="course-title">CSE115</a>
+                    <a href="https://drive.google.com/drive/folders/1TpMClETK7O3sLf6O_JAy2C0bXVs-rns_?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">CSE115</a>
                 </div>
                 <div  className="course glass">
-                    <a href="https://drive.google.com/file/d/1w5-49wbdVTu6slA6Oj2QLsii3oKdkpUx/view?usp=drive_link" target="_blank" className="course-title">CSE173</a>
+                    <a href="https://drive.google.com/drive/folders/1CBMO2UejzBZCzh0SQbNvC_21QUyd07xn?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">CSE173</a>
                 </div>
                 <div className="course glass">
-                    <a href="https://drive.google.com/file/d/19SHrpt3rfzU_nrUgGsPzE_X83G5rFCv5/view?usp=drive_link" target="_blank" className="course-title">MAT125</a>
+                    <a href="https://drive.google.com/drive/folders/1sqNg8-kQydcsflUApjb03Z2lZ35Byryi?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">MAT125</a>
                 </div>
                 <div className="course glass">
-                    <a href="https://drive.google.com/file/d/1i_jNJSjeunDs7wqTcW8n-8Kw8VPxeKKr/view?usp=drive_link" target="_blank" className="course-title">MAT130</a>
+                    <a href="https://drive.google.com/drive/folders/1VZ-mSsSQWZtFBr3qF2vqvl0wNrdXVyml?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">MAT130</a>
                 </div>
+                <div className="course glass">
+                    <a href="https://drive.google.com/drive/folders/1k6v-69dr-22QZJxCkAky7hsjFTxt-pQh?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">CSE225</a>
+                </div>
+                <div className="course glass">
+                    <a href="https://drive.google.com/drive/folders/1VmfXZyprc-NeY8wEhLuUaEeTpgm_ke2O?dmr=1&ec=wgc-drive-hero-goto" target="_blank" className="course-title">EEE141</a>
+                </div>
+                
             </div>
         </div>
     )
