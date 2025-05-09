3801078d1e28e97e699664e139e73cf6ebe64860
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-05-09 23:59:51 +0600

    .

--- personal-website/src/Projects.jsx
+++ personal-website/src/Projects.jsx
@@ -31,6 +31,34 @@ export default function Projects() {
           </div>
         </div>
       </div>
+      <div className="project-card glass">
+        <img src="cgpa.png" alt="GameStack Screenshot" />
+        <div className="project-info">
+          <h3>CGPA Calculator</h3>
+          <p>
+            A clean and efficient CGPA calculator app that helps students
+            accurately compute their cumulative GPA based on course grades and
+            credit hours.
+          </p>
+          <p>Tech Stack : React + Tailwind</p>
+          <div className="project-links">
+            <a
+              href="https://fahimmuntasr.github.io/CGPA.Calculator/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
+            <a
+              href="https://github.com/FahimMuntasr/CGPA.Calculator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
