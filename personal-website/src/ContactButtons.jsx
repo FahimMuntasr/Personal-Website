fe3c2dd4d3803f47981a05235aaebfdf30fa8c0c
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-07-08 16:41:50 +0600

    Update LinkedIn link

--- personal-website/src/ContactButtons.jsx
+++ personal-website/src/ContactButtons.jsx
@@ -1,16 +1,39 @@
-import './ContactButtons.css';
-export default function ContactButtons(){
-    return(
-        <div className="Contact-Buttons-Container ">
-            <a href="https:/www.linkedin.com/in/fahim-muntasir-profile" id="LinkedIn-Button" target="_blank"className="Contact-Buttons-Item ">
-                <img src="LinkedIn.png" id="LinkedIn-Icon" className="Contact-Buttons-Image"/>
-            </a>
-            <a href="https://github.com/FahimMuntasr" id="Github-Button" target="_blank" className="Contact-Buttons-Item">
-                <img src="Github.png" id="Github-Icon" className="Contact-Buttons-Image"/>
-            </a>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=byefahimmuntasir@gmail.com&su=Subject%20Here&body=Message%20content%20here" id="Mail-Button" target="_blank" className="Contact-Buttons-Item">
-                <img src="Mail.png" id="Mail-Icon" className="Contact-Buttons-Image"/>
-            </a>
-        </div>
-    )
+import "./ContactButtons.css";
+export default function ContactButtons() {
+  return (
+    <div className="Contact-Buttons-Container ">
+      <a
+        href="https://www.linkedin.com/in/fahim-muntasir-3b5a7b373/"
+        id="LinkedIn-Button"
+        target="_blank"
+        className="Contact-Buttons-Item "
+      >
+        <img
+          src="LinkedIn.png"
+          id="LinkedIn-Icon"
+          className="Contact-Buttons-Image"
+        />
+      </a>
+      <a
+        href="https://github.com/FahimMuntasr"
+        id="Github-Button"
+        target="_blank"
+        className="Contact-Buttons-Item"
+      >
+        <img
+          src="Github.png"
+          id="Github-Icon"
+          className="Contact-Buttons-Image"
+        />
+      </a>
+      <a
+        href="https://mail.google.com/mail/?view=cm&fs=1&to=fahim.muntasir.work@gmail.com&su=Subject%20Here&body=Message%20content%20here"
+        id="Mail-Button"
+        target="_blank"
+        className="Contact-Buttons-Item"
+      >
+        <img src="Mail.png" id="Mail-Icon" className="Contact-Buttons-Image" />
+      </a>
+    </div>
+  );
 }
